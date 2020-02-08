fd2aef789a823e27309a7015da4355e00ea8bab9
Cristian Nieto <cristianandresnieto@gmail.com>
2020-02-08 17:12:05 -0500

    Config production database

--- database/config/config.js
+++ database/config/config.js
@@ -28,10 +28,23 @@ module.exports =
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": "admin",
+    "password": "Pv5CFY6gcvHEp8wk",
+    "database": "branch",
+    "host": "databasec3xsmart.csw5vlnybfvb.us-east-2.rds.amazonaws.com",
+    "dialect": "mysql",
+    define: {
+      underscored: false,
+      freezeTableName: true
+    },
+    dialectOptions: {
+      typeCast: function (field, next) {
+        if (field.type == 'DATETIME' || field.type == 'TIMESTAMP') {
+          return new Date(field.string() + 'Z');
+        }
+        return next();
+      }
+    },
+    "timezone": '-05:00' //for writing to database
   }
 }
