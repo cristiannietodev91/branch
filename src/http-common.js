50c788fa9407d4aa7db2cf4e07f533751bfb0b28
Cristian Nieto <cristianandresnieto@gmail.com>
2020-02-08 20:53:09 -0500

    Ajuste despliegue

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { apiBranchUrl } from "./constants/config";
 
 //TODO: Pending load url from environment variable
-console.log('Backservcies URl :::>',apiBranchUrl) // eslint-disable-line no-console
+console.log('Backservcies URl ::::>',apiBranchUrl) // eslint-disable-line no-console
 
 export default axios.create({
   baseURL: apiBranchUrl,
