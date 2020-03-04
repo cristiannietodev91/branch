a12e3e18e64b0f5ffe12692bbfe1befe418671b7
Cristian Nieto <cristianandresnieto@gmail.com>
2020-03-04 13:06:10 -0500

    Allow cors

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 // server.js
 var express = require('express');
 var serveStatic = require('serve-static'); 
+var cors = require('cors');
 var history = require('connect-history-api-fallback');
 
 var app = express();
 
 app.use(history());
-
+app.use(cors())
 app.use(serveStatic(__dirname + "/dist")); 
 
 var port = process.env.PORT || 5000;
