a180e45b3d4293a273c626851718db9a399d6a85
cris.nieto.dafiti <cristian.nieto@dafiti.com.co>
2020-06-17 17:34:34 -0500

    Actualizacion de version y subidad version en package json

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,13 +1,13 @@
-export const defaultMenuType = 'menu-default' // 'menu-default', 'menu-sub-hidden', 'menu-hidden';
-export const defaultStartPath = '/app/dashboards/default'
-export const searchPath = '/app/pages/miscellaneous/search'
-export const buyUrl = 'https://1.envato.market/nEyZa'
+export const defaultMenuType = "menu-default"; // 'menu-default', 'menu-sub-hidden', 'menu-hidden';
+export const defaultStartPath = "/app/dashboards/default";
+export const searchPath = "/app/pages/miscellaneous/search";
+export const buyUrl = "https://1.envato.market/nEyZa";
 
-export const subHiddenBreakpoint = 1440
-export const menuHiddenBreakpoint = 768
+export const subHiddenBreakpoint = 1440;
+export const menuHiddenBreakpoint = 768;
 
-export const defaultLocale = 'es'
-export const defaultDirection = 'ltr'
+export const defaultLocale = "es";
+export const defaultDirection = "ltr";
 // export const localeOptions = [
 //   { id: 'en', name: 'English LTR', direction: 'ltr' },
 //   { id: 'es', name: 'Español', direction: 'ltr' },
@@ -24,30 +24,29 @@ export const firebaseConfig = {
   appId: "1:74838816843:web:5ca46e82f1587bc252b5ce"
 };
 
+export const apiUrl = "https://api.coloredstrategies.com";
 
-export const apiUrl = 'https://api.coloredstrategies.com'
-
-export const apiBranchUrl = process.env.VUE_APP_URLBACKSERVICES
+export const apiBranchUrl = process.env.VUE_APP_URLBACKSERVICES;
 
 export const currentUser = {
   id: 1,
-  title: 'Sarah Kortney',
-  img: '/assets/img/profile-pic-l.jpg',
-  date: 'Last seen today 15:24'
-}
+  title: "Sarah Kortney",
+  img: "/assets/img/profile-pic-l.jpg",
+  date: "Last seen today 15:24"
+};
 
-export const isDemo = true
-export const themeRadiusStorageKey = 'theme_radius'
-export const defaultColor = 'dark.blue'
+export const isDemo = true;
+export const themeRadiusStorageKey = "theme_radius";
+export const defaultColor = "light.blue";
 export const colors = [
   // 'light.purple',
   // 'dark.purple',
-  'light.blue',
-  'dark.blue',
+  "light.blue",
+  "dark.blue"
   // 'light.green',
   // 'dark.green',
   // 'light.orange',
   // 'dark.orange',
   // 'light.red',
   // 'dark.red'
-]
+];
