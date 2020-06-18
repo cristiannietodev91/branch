6dd098766f905bab93f7ef8e7bd4d777c0a26a0c
cris.nieto.dafiti <cristian.nieto@dafiti.com.co>
2020-06-17 22:22:01 -0500

    Deploy to AWS

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 // server.js
-var express = require('express');
-var serveStatic = require('serve-static'); 
-var cors = require('cors');
-var history = require('connect-history-api-fallback');
+var express = require("express");
+var serveStatic = require("serve-static");
+var cors = require("cors");
+var history = require("connect-history-api-fallback");
 
 var app = express();
 
 app.use(history());
-app.use(cors())
-app.use(serveStatic(__dirname + "/dist")); 
+app.use(cors());
+app.use(serveStatic(__dirname + "/dist"));
 
 var port = process.env.PORT || 5000;
-app.listen(port); 
-console.log('server started ' + port);
+app.listen(port);
+console.log("server started " + port);
