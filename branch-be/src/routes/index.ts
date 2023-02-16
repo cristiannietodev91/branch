e045bcf8d901e4cfc5df164318cf56bab9c1375b
Cristian Nieto <cristianandresnieto@gmail.com>
2023-02-15 21:13:37 -0500

    TCK-007 update web production docker image to handle routing and new health BE endpoint

--- branch-be/src/routes/index.ts
+++ branch-be/src/routes/index.ts
@@ -1,7 +1,12 @@
 import {Application , Request, Response }  from "express";
+import HttpStatus from "http-status-codes";
 
 export const register = ( app: Application ): void => {
-  app.get("/", (req: Request, res: Response, next) => {
+  app.get("/", (_: Request, res: Response) => {
     res.redirect("http://mybranchapp.com/");
   });
-}
+
+  app.get("/health", (_: Request, res: Response) => {
+    return res.status(HttpStatus.OK).json({ message: "healthy"});
+  });
+};
