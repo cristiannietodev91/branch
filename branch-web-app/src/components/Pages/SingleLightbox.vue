9facfa45d51c9a5568d6f16c55e48dfa21209e64
Cristian Nieto <42104272+cristiannietodev91@users.noreply.github.com>
2023-04-12 21:32:08 -0500

    TCK-15 implement light box gallery in workshop step view (#15)

---
+++ branch-web-app/src/components/Pages/SingleLightbox.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <span @click.native.prevent="onThumbClick(0)">
+      <img :src="thumb" alt="thumbnail" :class="className">
+    </span>
+    <LightGallery
+      :images="[large]"
+      :index="photoIndex"
+      :disable-scroll="true"
+      @close="handleHide()"
+    />
+  </div>
+</template>
+
+<script>
+import LightGallery from "../Gallery/LightGallery.vue";
+
+export default {
+    components: {
+      LightGallery
+    },
+    props: ["thumb", "large", "className"],
+    data() {
+        return {
+            photoIndex: null
+        };
+    },
+    methods: {
+        onThumbClick(index) {
+            this.photoIndex = index;
+        },
+        handleHide() {
+            this.photoIndex = null;
+        }
+    }
+};
+</script>
